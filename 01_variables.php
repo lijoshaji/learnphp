645a75a74517c92444fa68a2b406638c61e8c71f
Lijo s shaji <lijoshaji98@gmail.com>
2022-08-13 18:05:13 +0530

    Commit message

---
+++ 01_variables.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+echo 5 + 5;
+echo '<br>';
+$x = '5' + '5';
+var_dump($x);
+echo '<br>';
+echo 10 - 5;
+echo '<br>';
+echo 5 * 5 ;
+echo '<br>';
+echo 10 / 2;
+echo '<br>';
+echo 10 % 3;
+
+
+
+    ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -8,9 +8,32 @@
 </head>
 <body>
  <?php 
-echo "Hello World";
-echo "<br>";
-echo "<h1>Learn PHP<h1>";
+//echo 123,"Hello World",10.5;
+//echo "<br>";
+//echo "<h1>Learn PHP<h1>";
+//print_r([1,2,3]);
  ?>   
 </body>
-</html> 
+</html>
+
+<?php
+print_r([1,2,3]);
+var_dump('hello');
+var_dump(true);
+echo"<br>";
+var_export("hello");
+
+echo"<br>";
+
+$name = 'lijo';//string
+$age = '24';//int
+$has_kids = false;//bool
+$cash_on_hand = 500.50;//float
+
+echo $name . 'is' . $age . 'years old and lijo have' . $cash_on_hand . 'in hand';
+echo '<br>';
+echo "$name is $age years old and lijo have $cash_on_hand in hand";
+
+
+?>
+
